f425ec7825f4c1a4ab5e56a4cddc6ba71b993d08
YAxelY <mboumeu581@gmail.com>
2025-04-28 15:10:58 +0100

    flase test added

--- src/test/java/com/example/demoApp/DemoAppApplicationTests.java
+++ src/test/java/com/example/demoApp/DemoAppApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demoApp;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,10 @@ class DemoAppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+void shouldFail() {
+    assertTrue(false); // Test volontairement cassé
+}
 
 }
